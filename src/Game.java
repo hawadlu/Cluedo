b78e5759f8950616c385a78428dab5e64b9fd69f
Luke <hawadlu@outlook.com>
2020-07-27 15:20:52 +1200

    Adds basic code for showing an instructions screen.

--- src/Game.java
+++ src/Game.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args){
+        System.out.println("Welcome to Cluedo!\n" +
+                "To view the instructions type 'instructions'\n" +
+                "To play the game type 'play'");
+
+        String response = new Scanner(System.in).nextLine();
+
+        if (response.equals("instructions")) showInstructions();
+        else if (response.equals("play")) playGame();
+
         System.out.println("Please input something: ");
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLine());
+    }
+
+    /**
+     * Play the game
+     */
+    private static void playGame() {
+        System.out.println("You are playing the game");
+    }
+
+    /**
+     * Show the instructions
+     */
+    private static void showInstructions() {
+        //todo write the actual instructions
+        System.out.println("You are viewing the instructions");
     }
 
     public enum players {
