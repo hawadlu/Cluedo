30569623db4f5e46dedde6778ab0bbb514b57738
Destiro <connordebruyn234@gmail.com>
2020-07-27 12:57:52 +1200

    Made sum enum :)

--- src/Game.java
+++ src/Game.java
@@ -6,4 +6,34 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLine());
     }
+
+    public enum players {
+        SCARLET,
+        PLUM,
+        WHITE,
+        PEACOCK,
+        GREEN,
+        MUSTARD
+    }
+
+    public enum rooms{
+        KITCHEN,
+        BALLROOM,
+        STUDY,
+        BILLARD_ROOM,
+        CONSERVATORY,
+        DINING_ROOM,
+        HALL,
+        LIBRARY,
+        LOUNGE
+    }
+
+    public enum weapons{
+        CANDLESTICK,
+        DAGGER,
+        LEAD_PIPE,
+        REVOLVER,
+        ROPE,
+        SPANNER
+    }
 }
