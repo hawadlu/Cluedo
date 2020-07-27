f0b0a57600e895cad5b1b39bbf8202ffc45d1290
Destiro <connordebruyn234@gmail.com>
2020-07-27 14:40:38 +1200

    Room draft

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,6 @@
 public class Card {
+    @Override
+    public String toString(){
+        return "";
+    }
 }
--- src/RoomCard.java
+++ src/RoomCard.java
@@ -1,3 +1,26 @@
 public class RoomCard extends Card {
+    Game.rooms roomName;
+    public String name;
 
+    public enum roomChar{
+        KI,
+        BA,
+        ST,
+        BI,
+        CO,
+        DI,
+        HA,
+        LI,
+        LO
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //TO-DO
+    @Override
+    public String toString(){
+        return "";
+    }
 }
