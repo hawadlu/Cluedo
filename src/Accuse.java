bcbc24016557caa7832c7aa44df66ae3ade34a7e
Luke <hawadlu@outlook.com>
2020-07-28 14:56:55 +1200

    Adds base code to the accuse class.

--- src/Accuse.java
+++ src/Accuse.java
@@ -5,8 +5,31 @@
  *  -End game if correct, current player loses if incorrect
  */
 public class Accuse implements Action {
-    @Override
-    public void performAction(Player player, String action) {
+    RoomCard room;
+    PlayerCard suspect;
+    WeaponCard weapon;
+    Game game;
+    Player player;
 
+    /**
+     * Setup the accusation
+     */
+    Accuse(RoomCard room, PlayerCard suspect, WeaponCard weapon, Game game, Player player) {
+        this.room = room;
+        this.suspect = suspect;
+        this.weapon = weapon;
+        this.game = game;
+        this.player = player;
+    }
+
+    @Override
+    public boolean apply() {
+        //Check to see if the accused matches
+        if (!game.accuseList.contains(room)) player.hasLost = true;
+        else if (!game.accuseList.contains(weapon)) player.hasLost = true;
+        else if (!game.accuseList.contains(suspect)) player.hasLost = true;
+
+        //todo end the game for a a win
+        return false;
     }
 }
