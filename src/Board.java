6b0d6ffedb459367fe21f6677a595b89019b0f20
Justin Hanrahan <justin.s.hanrahan@gmail.com>
2020-07-28 14:59:40 +1200

    Adds stuff in Board, Tile, Room

--- src/Board.java
+++ src/Board.java
@@ -21,49 +21,58 @@ public class Board {
       ArrayList<Tile> Kitchen = new ArrayList<>();
       Kitchen = allocateTiles(Kitchen, 0,5,1,5);
       Kitchen = allocateTiles(Kitchen, 1,5,6,6);
-      room.put("Kitchen", new Room(Kitchen));
+      room.put("Kitchen", new Room(Kitchen, "Kitchen"));
 
       ArrayList<Tile> BallRoom = new ArrayList<>();
       BallRoom = allocateTiles(BallRoom, 10,13,1,1);
       BallRoom = allocateTiles(BallRoom, 8,15,2,7);
-      room.put("BallRoom", new Room(BallRoom));
+      room.put("BallRoom", new Room(BallRoom, "BallRoom"));
 
       ArrayList<Tile> Conservatory = new ArrayList<>();
       Conservatory = allocateTiles(Conservatory, 18,23,1,4);
       Conservatory = allocateTiles(Conservatory, 19,22,5,5);
-      room.put("Conservatory", new Room(Conservatory));
+      room.put("Conservatory", new Room(Conservatory, "Conservatory"));
 
       ArrayList<Tile> DinningRoom = new ArrayList<>();
       DinningRoom = allocateTiles(DinningRoom, 0,4,9,9);
       DinningRoom = allocateTiles(DinningRoom, 0,7,10,15);
-      room.put("DinningRoom", new Room(DinningRoom));
+      room.put("DinningRoom", new Room(DinningRoom, "DinningRoom"));
 
       ArrayList<Tile> BilliardRoom = new ArrayList<>();
       BilliardRoom = allocateTiles(BilliardRoom, 18,23,8,12);
-      room.put("BilliardRoom", new Room(BilliardRoom));
+      room.put("BilliardRoom", new Room(BilliardRoom, "BilliardRoom"));
 
       ArrayList<Tile> Library = new ArrayList<>();
       Library = allocateTiles(Library, 17,17,15,17);
       Library = allocateTiles(Library, 23,23,15,17);
       Library = allocateTiles(Library, 18,22,14,14);
       Library = allocateTiles(Library, 18,22,15,18);
-      room.put("Library", new Room(Library));
+      room.put("Library", new Room(Library, "Library"));
 
       ArrayList<Tile> Lounge = new ArrayList<>();
       Lounge = allocateTiles(Lounge, 0,6,19,23);
       Lounge = allocateTiles(Lounge, 0,5,24,24);
-      room.put("Lounge", new Room(Lounge));
+      room.put("Lounge", new Room(Lounge, "Lounge"));
 
       ArrayList<Tile> Hall = new ArrayList<>();
       Hall = allocateTiles(Hall, 9,14,18,24);
-      room.put("Hall", new Room(Hall));
+      room.put("Hall", new Room(Hall, "Hall"));
 
       ArrayList<Tile> Study = new ArrayList<>();
       Study = allocateTiles(Study, 17,23,21,23);
       Study = allocateTiles(Study, 18,23,24,24);
-      room.put("Study", new Room(Study));
+      room.put("Study", new Room(Study, "Study"));
    }
 
+   /**Helper method to set room tiles
+    *
+    * @param tiles
+    * @param x1
+    * @param x2
+    * @param y1
+    * @param y2
+    * @return
+    */
    public ArrayList<Tile> allocateTiles(ArrayList<Tile> tiles, int x1, int x2, int y1, int y2){
       for(int i=y1; i<=y2; i++){
          for(int j=x1; i<=x2; i++){
