bc43874487c80890d17beaeeec83a3e1e2ff2d52
Luke <hawadlu@outlook.com>
2020-07-28 14:37:14 +1200

    Adds basic move ability.

--- src/Board.java
+++ src/Board.java
@@ -7,4 +7,13 @@ public class Board {
 
    Tile[][] board;
 
+    /**
+     * Cheks to see if this position in the board class is valid.
+     * @param currentX the x pos
+     * @param currentY the y pos
+     * @return boolean indicating valid/invalid.
+     */
+    public boolean isValidPosition(int currentX, int currentY) {
+        return true; //todo implement me
+    }
 }
